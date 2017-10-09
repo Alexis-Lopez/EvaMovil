d772152dab78cb5cc2d930cf8970f5625d7531ee
Alexis-Lopez <alexis_tla_21@hotmail.com>
2017-10-09 01:28:15 -0500

    Usuario Conductor Diseno

--- app/src/main/java/com/platzi/evatransportes/ConductorActivity.java
+++ app/src/main/java/com/platzi/evatransportes/ConductorActivity.java
@@ -1,7 +1,13 @@
 package com.platzi.evatransportes;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.platzi.evatransportes.Fracments.ListaViajesFragment;
 
 public class ConductorActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class ConductorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conductor);
+      // showToolbar(getResources().getString(R.string.title_Conductor),false);
+        ListaViajesFragment listaViajesFragment = new ListaViajesFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content,listaViajesFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
     }
+
+   /* public void showToolbar(String title,boolean upButton){
+        Toolbar toolbar = (Toolbar)  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }*/
+
+
 }
