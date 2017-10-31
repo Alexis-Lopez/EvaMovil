3b777012d342bb608689012d411e371c2868d806
Alexis-Lopez <alexis_tla_21@hotmail.com>
2017-10-31 17:04:13 -0500

    Mapa para los viajes

---
+++ app/src/main/java/com/platzi/evatransportes/ViajesActivity.java
@@ -0,0 +1,103 @@
+package com.platzi.evatransportes;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ViajesActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
+
+    private GoogleMap mMap;
+    public double latitude;
+    public double longitude;
+    public LocationManager locationManager;
+    public Criteria criteria;
+    public String bestProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viajes);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        getLocation();
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMinZoomPreference(14);
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void getLocation(){
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        criteria =  new Criteria();
+        bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            //Toast.makeText(App.getContext(), "GL latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_LONG).show();
+
+        }
+        else{
+            //This is what you need:
+            locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+        }
+    }
+
+}
--- app/src/main/java/com/platzi/evatransportes/View/LoginActivity.java
+++ app/src/main/java/com/platzi/evatransportes/View/LoginActivity.java
@@ -1,14 +1,19 @@
 package com.platzi.evatransportes.View;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.platzi.evatransportes.ClienteActivity;
 import com.platzi.evatransportes.ConductorActivity;
+import com.platzi.evatransportes.Model.Conductor;
 import com.platzi.evatransportes.R;
 import com.platzi.evatransportes.TransportistaActivity;
+import com.platzi.evatransportes.ViajesActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,18 +23,29 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void mostrarViajesalConductor(View view){
-        Intent intent = new Intent(this,ConductorActivity.class);
-        startActivity(intent);
-    }
+    public void usuarios(View view){
+        TextInputEditText tvUser = (TextInputEditText) findViewById(R.id.Username);
+        TextInputEditText tvPassword = (TextInputEditText) findViewById(R.id.Password);
 
-    public void mostrarTransportista(View view){
-        Intent intent =  new Intent(this, TransportistaActivity.class);
-        startActivity(intent);
-    }
-    public void mostrarViajesCliente(View view){
-        Intent intent = new Intent(this, ClienteActivity.class);
-        startActivity(intent);
+        String User,pass;
+
+        User = tvUser.getText().toString().trim();
+        pass = tvPassword.getText().toString().trim();
+
+        if (User.compareTo("conductor@hotmail.com") == 0 && pass.compareTo("123456")==0){
+                Intent intent = new Intent(this, ConductorActivity.class);
+                startActivity(intent);
+        }
+        else if(User.compareTo("cliente@hotmail.com")==0 && pass.compareTo("123456")==0){
+                Intent intent = new Intent(this,ClienteActivity.class);
+                startActivity(intent);
+        }else if(User.compareTo("transportista@hotmail.com")==0 && pass.compareTo("123456")==0){
+            Intent intent = new Intent(this,TransportistaActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this,"El usuario o la contraseña esta equivocada  "+User+" "+pass +" ", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
